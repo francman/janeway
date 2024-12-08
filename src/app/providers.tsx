a92772a391d3ee85fb1d50d53bcaa59511146ebd
control11011 <frankmanu500@gmail.com>
2024-12-08 01:03:03 -0500

    added analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,25 @@
 import { createContext, useEffect, useRef } from 'react'
 import { usePathname } from 'next/navigation'
 import { ThemeProvider, useTheme } from 'next-themes'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import dynamic from 'next/dynamic'
+
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
+  ssr: false,
+})
+
+export function PHProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: 'identified_only',
+      capture_pageview: false,
+      capture_pageleave: true, // Enable pageleave capture
+    })
+  }, [])
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+}
 
 function usePrevious<T>(value: T) {
   let ref = useRef<T>()
@@ -47,8 +66,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AppContext.Provider value={{ previousPathname }}>
       <ThemeProvider attribute="class" disableTransitionOnChange>
-        <ThemeWatcher />
-        {children}
+        <PHProvider>
+          <ThemeWatcher />
+          <PostHogPageView />
+          {children}
+        </PHProvider>
       </ThemeProvider>
     </AppContext.Provider>
   )
